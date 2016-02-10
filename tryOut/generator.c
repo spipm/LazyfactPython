8e65985b321c3173d0d348453efe8fa6bb5cf677
DutchGraa <koffieprog@gmail.com>
2016-02-10 21:52:26 +0100

    Cleaned generator.c a little
    Removed useless old parts of the generator.c code.

--- tryOut/generator.c
+++ tryOut/generator.c
@@ -1,26 +1,14 @@
 /* This file generates RSA moduli using OpenSSL */
 
-#include <gmp.h>
 #include <stdio.h>
-
 #include <openssl/rsa.h>
 
-#define OPENSSLGENBITS 128	// generate 128 bit moduli
+#define OPENSSLGENBITS 64	// generate 128 bit moduli
 #define OPENSSLGENEXPONENT 65537
 #define OPENSSLGENMAX 1000
 
-
-
 int main()
 {
-	mpz_t genN, tmp;
-	long counter = 0;
-	int status = -1;
-
-	//printf("[INFO ] Trying OpenSSL generating\n");
-
-	mpz_init(genN);		mpz_init(tmp);
-
 	BIGNUM *e = NULL;
 	RSA *rsa = NULL;
 
@@ -28,27 +16,20 @@ int main()
 	e = BN_new();
 	BN_set_word(e, OPENSSLGENEXPONENT);
 
+	long counter = 0;
 	while (counter < OPENSSLGENMAX)
 	{
 
 		if (RSA_generate_key_ex(rsa, OPENSSLGENBITS, e, NULL) == 0)
 			break;
 
-		mpz_set_str(genN, BN_bn2dec(rsa->n), 10);
-
-		printf("%s\n", mpz_get_str (0, 10, genN));
+		printf("%s\n", BN_bn2dec(rsa->n));
 
 		counter += 1;
 	}
 
-
 	BN_free(e);
 	RSA_free(rsa);
 
-	mpz_clear(genN);
-
-	return status;
+	return 0;
 }
-
-
-
